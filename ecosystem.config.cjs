24fadf18378b19d289a795a17f53e313fd8446b8
lizardspace2 <pierreduponddelabrouettet@proton.me>
2026-03-27 06:14:10 +0100

    eeee

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,13 +3,17 @@ module.exports = {
         {
             name: 'quantix-node',
             script: 'src/main.js',
+            node_args: '--max-old-space-size=1536',
             env: {
                 HTTP_PORT: 3001,
                 P2P_PORT: 6001,
-                PEERS: 'ws://35.225.236.73:6001,ws://136.115.214.0:6001'
+                PEERS: 'ws://35.225.236.73:6001',
+                ENABLE_MINING: 'false',
+                SAFE_MODE: 'true'
             },
             restart_delay: 5000,
-            max_restarts: 10,
+            exp_backoff_restart_delay: 100,
+            max_restarts: 30,
             time: true
         }
     ]
